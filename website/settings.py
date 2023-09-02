0969f90d4885ba1c1a76695b530ba6e786d217a5
Yanish <yanish.rambocus@ovo.com>
2023-09-02 17:17:04 +0100

    visitor??

--- website/settings.py
+++ website/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'visitor/static/')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = "afsdjango@gmail.com"
