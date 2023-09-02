4a9a180c71d48d92e4300f7ee3a5f3126033f8f5
Yanish <yanish.rambocus@ovo.com>
2023-09-02 16:26:22 +0100

    put visitors app back oin

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visitor.apps.VisitorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
