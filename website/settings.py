d4b47e0c7cdb4eaa1b6e6e2c139b68d161cc501a
Yanish <yanish.rambocus@ovo.com>
2023-09-14 22:20:40 +0100

    add other env vars

--- website/settings.py
+++ website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gbgk=6o*3c95w2aojgs82uyyz7+#om*j4ox^x#%3!&b^h7yd&3'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
