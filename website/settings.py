9217309741b9b779bbd5b031f7169f6b027937a8
Yanish Rambocus <yanishrambocus@gmail.com>
2023-09-14 22:47:54 +0100

    use config vars (#17)

--- website/settings.py
+++ website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gbgk=6o*3c95w2aojgs82uyyz7+#om*j4ox^x#%3!&b^h7yd&3'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,11 +103,11 @@ STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "static/"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = "afsdjango@gmail.com"
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "nypjuw-jygza2-nuMkiv"
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
