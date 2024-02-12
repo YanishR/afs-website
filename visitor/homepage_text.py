3b007779c645c2cb71862fdf8eae5b3e058417b0
Yanish Rambocus <yanishrambocus@gmail.com>
2024-02-12 21:37:00 +0400

    Amend text appropriately (#24)

--- visitor/homepage_text.py
+++ visitor/homepage_text.py
@@ -4,21 +4,18 @@ import dataclasses
 @dataclasses.dataclass
 class HomepageText:
     our_firm: str =  """
-        Located in Floreal, the Financial Services Centre of Mauritius, we provide an array of services and 
-        solutions that help our clients run their businesses efficiently by optimising their business processes, 
-        improving their reporting accuracy and minimising their operating costs. Ranging from Accountancy, Structuring 
+        Located in Floreal, an affluent town home to foreign consulates and embassies, we provide Accountancy services and 
+        solutions that enable our clients to run their businesses efficiently. Ranging from Accountancy, Structuring 
         and Tax Services, Formation of Companies, Corporate Secretarial and other Financial Services to Individuals and 
         Corporates, we offer a professional and personalised service on a case-to-case basis to our clients.
     """
     about_us: str = """
-        Abhasa Financial Services originated from the experience and know-how of professionals in the field 
-        of finance and technology. Based in Ébène, the new financial hub of Mauritius, we act as a strategic 
-        financial business partner.
+        Abhasa Financial Services has a team of professionals dedicated to service the clients by ensuring that all regulatory obligations are met and offer guidance on the financial aspects of their businesses. The clients are mainly family owned enterprises operating in a wide range of activities. AFS has gained valuable experience in advising both foreign and mauritian nationals to set up businesses in the domestic market. The team assist in the incorporation process, bank account opening and set up the accounting framework.
     """
     resume: str =  """ 
-        Rubysen Rambocus, Director and Shareholder of Abhasa Limited, is a member of the Institute of Chartered
-        Accountants of England and Wales. He has acquired strong commercial and business acumen from years of senior
-        managerial positions with major international conglomerates, as a result of operating in diverse and dynamic sectors
-        such as financial services, telecommunications, manufacturing and distribution. Rubysen has successfully established
-        numerous business ventures contributing to creation of employment. 
+        Rubysen Rambocus, founder of Abhasa, is a member of the Institute of Chartered Accountants of England and Wales. 
+        He has acquired strong commercial and business acumen from years of senior
+        managerial positions with major international conglomerates, operating in diverse and dynamic sectors
+        such as financial services, telecommunications, manufacturing and distribution. The business experience gained enables him to offer insightful guidance and advice to the clients of the company.
+         
     """
