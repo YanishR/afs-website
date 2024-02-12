f7bcff383a0a21518bcc5b3837e902a1cc73335c
Yanish <yanishrambocus@gmail.com>
2024-02-12 21:07:25 +0400

    change more text

--- visitor/homepage_text.py
+++ visitor/homepage_text.py
@@ -10,9 +10,7 @@ class HomepageText:
         Corporates, we offer a professional and personalised service on a case-to-case basis to our clients.
     """
     about_us: str = """
-        Abhasa Financial Services originated from the experience and know-how of professionals in the field 
-        of finance and technology. Based in Ébène, the new financial hub of Mauritius, we act as a strategic 
-        financial business partner.
+        Abhasa Financial Services has a team of professionals dedicated to service the clients by ensuring that all regulatory obligations are met and offer guidance on the financial aspects of their businesses. The clients are mainly family owned enterprises operating in a wide range of activities. AFS has gained valuable experience in advising both foreign and mauritian nationals to set up businesses in the domestic market. The team assist in the incorporation process, bank account opening and set up the accounting framework.
     """
     resume: str =  """ 
         Rubysen Rambocus, founder of Abhasa, is a member of the Institute of Chartered Accountants of England and Wales. 
